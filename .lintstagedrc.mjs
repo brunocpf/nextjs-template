ea2dcac557a4ba313e6da40c375046821379092b
Bruno Fernandes <brunocpf@outlook.com>
2024-12-28 17:49:32 -0300

    Update linting commands for better debugging

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,11 +3,19 @@ import { relative } from "path";
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => relative(process.cwd(), f))
+    .map((f) => {
+      console.log(f);
+      return f;
+    })
     .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
   `prettier --write ${filenames
     .map((f) => relative(process.cwd(), f))
+    .map((f) => {
+      console.log(f);
+      return f;
+    })
     .join(" ")}`;
 
 const lintStagedConfig = {
