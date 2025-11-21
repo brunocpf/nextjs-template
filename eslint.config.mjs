8bbb9547e0af3ac37da2ef5f6f92b716c0625fc0
Bruno Fernandes <brunocpf@outlook.com>
2025-11-21 02:54:09 -0300

    refactor: update ESLint configuration and improve naming conventions in components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,32 @@
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 import checkFile from "eslint-plugin-check-file";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"), {
-  plugins: {
-    "check-file": checkFile,
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "src/**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/": "KEBAB_CASE",
+        },
+      ],
+    },
   },
-  rules: {
-    "check-file/filename-naming-convention": [
-      "error",
-      {
-        "src/**/*.{ts,tsx}": "KEBAB_CASE",
-      },
-    ],
-    "check-file/folder-naming-convention": [
-      "error",
-      {
-        "src/**/": "KEBAB_CASE",
-      },
-    ],
-  },
-}];
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
 
 export default eslintConfig;
